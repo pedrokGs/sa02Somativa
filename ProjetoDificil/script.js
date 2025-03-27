2675d65e1b87cbcb55122a73ab3f2243de86e5fc
PedroHGon <pedibr5@gmail.com>
2025-03-27 10:43:43 -0300

    27.03.2025 - "Estilização básica (unfinished)"

--- ProjetoDificil/script.js
+++ ProjetoDificil/script.js
@@ -1,3 +1,7 @@
+let textResultado = document.getElementById('resultado');
+let escolhaComputer = document.getElementById('escolhaComputer');
+let escolhaUser = document.getElementById('escolhaUser');
+
 function randomChoice() {
   let escolhas = ["pedra", "papel", "tesoura"];
   const escolha = Math.floor(Math.random() * escolhas.length);
@@ -13,7 +17,7 @@ function checkClash(c1, c2) {
   if (
     (c1 === "tesoura" && c2 === "papel") ||
     (c1 === "papel" && c2 === "pedra") ||
-    (c1 === "pedra " && c2 === "tesoura")
+    (c1 === "pedra" && c2 === "tesoura")
   ) {
     return "Vitória!";
   } else {
@@ -23,11 +27,16 @@ function checkClash(c1, c2) {
 }
 
 function gameStart(c1) {
-  c2 = randomChoice();
+  let c2 = randomChoice();
   
   console.log("Você escolheu: " + c1);
   console.log("O computador escolheu: " + c2);
-  console.log("Resultado: " + checkClash(c1,c2));
+  let resultado = checkClash(c1,c2);
+  console.log("Resultado: " + resultado);
+
+  escolhaComputer.src = "../assets/img/" + c2 + ".png";
+  escolhaUser.src = "../assets/img/" + c1 + ".png";
+  textResultado.textContent = resultado;
 }
 
 gameStart('pedra');
