eb4420e3f526f3ccce2c36fc17130b67bd54eb54
PedroHGon <pedibr5@gmail.com>
2025-03-27 11:07:47 -0300

    27.03.2025 - "Finalização de estilização" - "Começando segundo modo"

--- ProjetoDificil/script.js
+++ ProjetoDificil/script.js
@@ -3,25 +3,24 @@ let escolhaComputer = document.getElementById('escolhaComputer');
 let escolhaUser = document.getElementById('escolhaUser');
 
 function randomChoice() {
-  let escolhas = ["pedra", "papel", "tesoura"];
+  let escolhas = ["Pedra", "Papel", "Tesoura"];
   const escolha = Math.floor(Math.random() * escolhas.length);
   return escolhas[escolha];
 }
 
 function checkClash(c1, c2) {
   if (c1 === c2) {
-    console.log("Empate!");
-    return "Empate!";
+    return "empate";
   }
 
   if (
-    (c1 === "tesoura" && c2 === "papel") ||
-    (c1 === "papel" && c2 === "pedra") ||
-    (c1 === "pedra" && c2 === "tesoura")
+    (c1 === "Tesoura" && c2 === "Papel") ||
+    (c1 === "Papel" && c2 === "Pedra") ||
+    (c1 === "Pedra" && c2 === "Tesoura")
   ) {
-    return "Vitória!";
+    return "vitoria";
   } else {
-    return "Derrota!";
+    return "derrota";
     
   }
 }
@@ -34,16 +33,32 @@ function gameStart(c1) {
   let resultado = checkClash(c1,c2);
   console.log("Resultado: " + resultado);
 
-  escolhaComputer.src = "../assets/img/" + c2 + ".png";
-  escolhaUser.src = "../assets/img/" + c1 + ".png";
-  textResultado.textContent = resultado;
+
+  updateImage('escolhaComputer', `../assets/img/${c2}.png`);
+  updateImage('escolhaUser', `../assets/img/${c1}.png`);
+  //escolhaComputer.src = "../assets/img/" + c2 + ".png";
+  //escolhaUser.src = "../assets/img/" + c1 + ".png";
+
+  if (resultado === 'vitoria')
+    textResultado.innerHTML = `${c1} ganha de ${c2}! <br> Você venceu!`;
+  else if (resultado === 'empate')
+    textResultado.innerHTML = `${c1} empata com ${c2}! <br> Empate!`;
+  else
+    textResultado.innerHTML = `${c1} perde para ${c2}! <br> Você perdeu!`;
+
+
+
+
 }
 
-gameStart('pedra');
-gameStart('pedra');
-gameStart('papel');
-gameStart('tesoura');
-gameStart('tesoura');
-gameStart('papel');
-gameStart('pedra');
-gameStart('tesoura');
+function updateImage(containerId, newImageSrc) {
+  const imgElement = document.getElementById(containerId);
+
+  imgElement.classList.add('hidden');
+
+
+  setTimeout(() => {
+    imgElement.src = newImageSrc;
+    imgElement.classList.remove('hidden');
+  }, 500);
+}
